b55e57dd3c45831bd1c68e740075602970eafee8
AndrewsGilbert <tandrewsgilbert@gmail.com>
2021-07-12 23:07:45 +0530

    news creation & audio generation seperated as functions & recursion used in audio  generation

--- test.ts
+++ test.ts
@@ -35,12 +35,13 @@ type content = {
     newsObject:Array<news>
 }
 
-cron.schedule('23 9 * * *', function () {
+cron.schedule('58 22 * * *', function () {
 
     const content: string = fs.readFileSync('ref.json', 'utf8')
     const contentJson: content = JSON.parse(content)
     const web: Array<web> = contentJson.web
     const newsObject: Array<news> = contentJson.newsObject
+    const date:string = new Date().toString().replace(/[{(+)}]|GMT|0530|India Standard Time| /g, '')
 
     for (let i: number = 0; i < web.length; i++) {
 
@@ -53,7 +54,6 @@ cron.schedule('23 9 * * *', function () {
 
               let news: Array<newsContent> = []
          
-  
                 for (let j: number = 0; j < selector.length; j++) {
         
                   const selectorId: string = selector[j]
@@ -63,39 +63,64 @@ cron.schedule('23 9 * * *', function () {
                   
                   let newsDetail = <newsContent>{}
                   newsDetail.text = data
-                  const fileName = 'voice/id:' + id + ',' + j + '.wav'
+                  const fileName = 'voice/webId:'+id+'-ind:'+j+'-'+date+'.wav'
                   newsDetail.audio = fileName
                   news[j] = newsDetail
                   
-                   exec(`./tts --text "${data}" --out_path ${fileName}`, (error, stdout, stderr) => {
-                      if (error) {
-                        console.log(`error: ${error.message}`)
-                        return
-                      }
-                      if (stderr) {
-                        console.log(`stderr: ${stderr}`)
-                        return
-                      }
-                      console.log(`stdout: ${stdout}`)
-                    }) 
-                  
-
                 }
 
-                const index = newsObject.length
-                let detail = <news>{}
-                detail.webId = id
-                detail.newsDet = news
-                newsObject[index] = detail
+              const index = newsObject.length
+              let detail = <news>{}
+              detail.webId = id
+              detail.newsDet = news
+              newsObject[index] = detail
             
-                fs.writeFileSync('ref.json', JSON.stringify(contentJson, null, 2), 'utf8')
+              fs.writeFileSync('ref.json', JSON.stringify(contentJson, null, 2), 'utf8')
+              if (i === web.length-1) { audioGeneration() }
             }
         })
     }
 })
 
+function audioGeneration() {
+  const content: string = fs.readFileSync('ref.json', 'utf8')
+  const contentJson: content = JSON.parse(content)
+  const newsObject: Array<news> = contentJson.newsObject
 
+  audioGen()
+    
+  function audioGen(webInd: number = 0, newsInd = 0) {
+
+    const newsCollection: Array<newsContent> = newsObject[webInd].newsDet
+    const data = newsCollection[newsInd].text
+    const fileName = newsCollection[newsInd].audio
+    exec(`./tts --text "${data}" --out_path ${fileName}`, (error, stdout, stderr) => {
+
+      if (error) {
+        console.log(`error: ${error.message}`)
+        return
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`)
+        return
+      }
+      if (stdout) {
+
+        if (newsInd < newsCollection.length - 1) {
+          newsInd++
+          audioGen(webInd, newsInd)
+        }
+        if (newsInd === newsCollection.length - 1 && webInd < newsObject.length - 1) {
+          newsInd = 0
+          webInd++
+          audioGen(webInd, newsInd)
+        }
+        console.log(`stdout: ${stdout}`) 
+      }
+    })
+  }
+}
   
- app.listen(8586, function () {
-  console.log('Node server is running 8586..')
+ app.listen(8588, function () {
+  console.log('Node server is running 8588..')
 })
