87dcfbfa14bfbf61e5af1e98d03e29fe4dc86878
AndrewsGilbert <tandrewsgilbert@gmail.com>
2021-07-15 22:10:58 +0530

    properly called chain of promise

--- test.ts
+++ test.ts
@@ -40,7 +40,7 @@ type content = {
     newsObject:Array<news>
 }
 
-cron.schedule('18 18 * * *', function () {
+cron.schedule('29 21 * * *', function () {
 
     const content: string = fs.readFileSync('ref.json', 'utf8')
     const contentJson: content = JSON.parse(content)
@@ -100,18 +100,18 @@ function generate() {
   const contentJson: content = JSON.parse(content)
   const web: Array<web> = contentJson.web
   const newsObject: Array<news> = contentJson.newsObject
-
   const index: number = newsObject.length - web.length
+  let objectInd:number = index
+  let newsInd:number  = 0
 
-  audioGen()
-  
-  function audioGen(objectInd: number = index, newsInd = 0) {
+ 
+  let audioGen = function ():Promise<string> {
 
     const newsCollection: Array<newsContent> = newsObject[objectInd].newsDet
     const data = newsCollection[newsInd].text
     const fileName = newsCollection[newsInd].audio
 
-    let myPromise = new Promise((resolve, reject) => {
+    let myPromise = new Promise<string>((resolve, reject) => {
 
       if (objectInd < newsObject.length && newsInd < newsCollection.length) {
 
@@ -127,34 +127,70 @@ function generate() {
           }
           if (stdout) {
             console.log(`stdout: ${stdout}`)
-            resolve('Processing')
+            const data1:string = fileName
+            resolve(data1)
           }
         })
       }
     })
+    return myPromise
+  }
 
-    myPromise.then(() => {
-      
-      load(fileName).then(function (res: { duration: number }) {
-        newsCollection[newsInd].duration = res.duration
-        fs.writeFileSync('ref.json', JSON.stringify(contentJson, null, 2), 'utf8')
-      }).then(() => {
-          
-          if (newsInd < newsCollection.length - 1) {
+  let audioDuration = function(data1:string):Promise<string> {
+
+    console.log(1)
+    const fileName:string = data1
+    let myPromise = new Promise<string>((resolve, reject) => {
+
+        load(fileName).then(function (res: { duration: number }) {
+            const duration = res.duration
+            const newsCollection: Array<newsContent> = newsObject[objectInd].newsDet
+            newsCollection[newsInd].duration = duration
+            console.log(2)
+            resolve("duration got")
+        })
+    })
+    return myPromise
+  }
+
+  let write = function():Promise<string> {
+    console.log(3)
+    let myPromise = new Promise<string>((resolve, reject) => {
+        console.log(4)
+        fs.writeFile('ref.json',JSON.stringify(contentJson, null, 2) , function (err) {
+            if (err) throw err
+            console.log(5)
+            resolve('duration wrote')
+        })   
+    })
+    return myPromise
+  }
+
+  let recur = function () {
+    console.log(6)
+
+    const newsCollection: Array<newsContent> = newsObject[objectInd].newsDet
+
+        if (newsInd < newsCollection.length - 1) {
+            console.log(7,1)
             newsInd++
-            audioGen(objectInd, newsInd)
-          } else if (objectInd < newsObject.length - 1) {
+            audioGen().then(audioDuration).then(write).then(recur)
+        } else if (objectInd < newsObject.length - 1) {
+            console.log(7,2)
             newsInd = 0
             objectInd++
-            audioGen(objectInd, newsInd)
-          } else {
+            audioGen().then(audioDuration).then(write).then(recur)
+        } else {
+            console.log(7,3)
             console.log('All text news are converted to audio')
-          }
-        })
-    }).catch((message) => {
-        console.log(message);
-    })
+        }
+       
+ 
   }
+
+  audioGen().then(audioDuration).then(write).then(recur)
+
+
 }
   
 app.listen(8588, function () {
