333683f5ea9b0c3a22aa8875c2d360b5efc28f4f
AndrewsGilbert <tandrewsgilbert@gmail.com>
2021-07-15 12:47:46 +0530

    request type converted to promise return

--- promise.ts
+++ promise.ts
@@ -11,9 +11,10 @@ const url:string = 'https://www.goldpricesindia.com/'
 test()
 
 function test() {
-    
-    let myPromise = new Promise((resolve, reject) => {
-        exec(`./tts --text "${word}" --out_path ${fileName}`, (error, stdout, stderr) => {
+    console.log(1)
+    let myPromise1 = new Promise((resolve, reject) => {
+        console.log(2)
+      exec(`./tts --text "${word}" --out_path ${fileName}`, (error, stdout, stderr) => {
 
           if (error) {
             console.log(`error: ${error.message}`)
@@ -25,23 +26,42 @@ function test() {
           }
           if (stdout) {
             console.log(`stdout: ${stdout}`)
-            resolve('Processing')
+            console.log(3)
+            resolve('Processing myPromise1')
           }
         })
     })
 
-    myPromise.then(() => {
+  myPromise1.then(() => {
+    console.log(4)
+      let data:any
+    let myPromise2 = new Promise((resolve, reject) => {
+
+      console.log(5)
           request(url, function (err, resp, html) {
             if (!err && resp.statusCode === 200) {
                 const $ = cheerio.load(html)
                 const dataPath = $('.no-js')
-                const data: any = dataPath.html()
-                fs.writeFileSync('ref.txt',data, 'utf8')
-                const content: string = fs.readFileSync('ref.txt', 'utf8')
-                console.log(content)
+              data = dataPath.html()
+              console.log(6)
+              resolve('Processing myPromise2')
+
             }
-        })
+          })
+    })
+    
+    myPromise2.then(() => {
+      console.log(7)
+      fs.writeFileSync('ref.txt', data, 'utf8')
+    }).then(() => {
+      const content: string = fs.readFileSync('ref.txt', 'utf8')
+      console.log(8)
+      console.log(content)
+    })
     })
 }
 
-
+/*
+                fs.writeFileSync('ref.txt',data, 'utf8')
+                const content: string = fs.readFileSync('ref.txt', 'utf8')
+                console.log(content) */
